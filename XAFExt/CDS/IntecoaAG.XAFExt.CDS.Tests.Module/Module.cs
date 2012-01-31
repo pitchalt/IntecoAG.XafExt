7b319e47963c97d639ff6f802c3af6a2644387c7
dr.scientist <dr.scientist@608d0101-f27c-442d-b934-2f8edab33c71>
2012-01-31 17:24:06 +0000

    Нулевой цикл. Просто проекты без наполнения.

---
+++ XAFExt/CDS/IntecoaAG.XAFExt.CDS.Tests.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace IntecoaAG.XAFExt.CDS.Tests.Module {
+    public sealed partial class TestsModule : ModuleBase {
+        public TestsModule() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XAFExt/CDS/IntecoaAG.XAFExt.CDS/Module.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+
+namespace IntecoaAG.XAFExt.CDS {
+    public sealed partial class CDSModule : ModuleBase {
+        public CDSModule() {
+            InitializeComponent();
+        }
+    }
+}
