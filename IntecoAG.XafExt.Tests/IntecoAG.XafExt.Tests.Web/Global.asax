c90aacbb27aa61997ff3bdcc1888c72ec2790325
Uzorin Pavel <puzorin@yandex.ru>
2020-04-11 19:12:38 +0300

    Добавил модуль для тестирования Web

---
+++ IntecoAG.XafExt.Tests/IntecoAG.XafExt.Tests.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="IntecoAG.XafExt.Tests.Web.Global"%>
---
+++ IntecoAG.XafExt.Tests/IntecoAG.XafExt.Tests.Web/WebApplication.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Xpo;
+
+namespace IntecoAG.XafExt.Tests.Web {
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Web.WebApplication
+    public partial class TestsAspNetApplication : WebApplication {
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private IntecoAG.XafExt.Tests.Module.TestsModule module3;
+        private IntecoAG.XafExt.Tests.Module.Web.TestsAspNetModule module4;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.PivotChart.PivotChartModuleBase pivotChartModuleBase;
+        private DevExpress.ExpressApp.PivotChart.Web.PivotChartAspNetModule pivotChartAspNetModule;
+        private DevExpress.ExpressApp.PivotGrid.PivotGridModule pivotGridModule;
+        private DevExpress.ExpressApp.PivotGrid.Web.PivotGridAspNetModule pivotGridAspNetModule;
+        private DevExpress.ExpressApp.ReportsV2.ReportsModuleV2 reportsModuleV2;
+        private DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2 reportsAspNetModuleV2;
+        private DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase treeListEditorsModuleBase;
+        private DevExpress.ExpressApp.TreeListEditors.Web.TreeListEditorsAspNetModule treeListEditorsAspNetModule;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule validationAspNetModule;
+        private DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule viewVariantsModule;
+
+        #region Default XAF configuration options (https://www.devexpress.com/kb=T501418)
+        static TestsAspNetApplication() {
+			EnableMultipleBrowserTabsSupport = true;
+            DevExpress.ExpressApp.Web.Editors.ASPx.ASPxGridListEditor.AllowFilterControlHierarchy = true;
+            DevExpress.ExpressApp.Web.Editors.ASPx.ASPxGridListEditor.MaxFilterControlHierarchyDepth = 3;
+            DevExpress.ExpressApp.Web.Editors.ASPx.ASPxCriteriaPropertyEditor.AllowFilterControlHierarchyDefault = true;
+            DevExpress.ExpressApp.Web.Editors.ASPx.ASPxCriteriaPropertyEditor.MaxHierarchyDepthDefault = 3;
+            DevExpress.Persistent.Base.PasswordCryptographer.EnableRfc2898 = true;
+            DevExpress.Persistent.Base.PasswordCryptographer.SupportLegacySha512 = false;
+        }
+        private void InitializeDefaults() {
+            LinkNewObjectToParentImmediately = false;
+            OptimizedControllersCreation = true;
+            DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2.EnableValueManagerInHtml5DocumentViewer = true;
+        }
+        #endregion
+        public TestsAspNetApplication() {
+            InitializeComponent();
+			InitializeDefaults();
+        }
+        protected override IViewUrlManager CreateViewUrlManager() {
+            return new ViewUrlManager();
+        }
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(GetDataStoreProvider(args.ConnectionString, args.Connection), true);
+            args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+        }
+        private IXpoDataStoreProvider GetDataStoreProvider(string connectionString, System.Data.IDbConnection connection) {
+            System.Web.HttpApplicationState application = (System.Web.HttpContext.Current != null) ? System.Web.HttpContext.Current.Application : null;
+            IXpoDataStoreProvider dataStoreProvider = null;
+            if(application != null && application["DataStoreProvider"] != null) {
+                dataStoreProvider = application["DataStoreProvider"] as IXpoDataStoreProvider;
+            }
+            else {
+                dataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, connection, true);
+                if(application != null) {
+                    application["DataStoreProvider"] = dataStoreProvider;
+                }
+            }
+			return dataStoreProvider;
+        }
+        private void TestsAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if(System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else {
+				string message = "The application cannot connect to the specified database, " +
+					"because the database doesn't exist, its version is older " +
+					"than that of the application or its schema does not match " +
+					"the ORM data model structure. To avoid this error, use one " +
+					"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new IntecoAG.XafExt.Tests.Module.TestsModule();
+            this.module4 = new IntecoAG.XafExt.Tests.Module.Web.TestsAspNetModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.pivotChartModuleBase = new DevExpress.ExpressApp.PivotChart.PivotChartModuleBase();
+            this.pivotChartAspNetModule = new DevExpress.ExpressApp.PivotChart.Web.PivotChartAspNetModule();
+            this.pivotGridModule = new DevExpress.ExpressApp.PivotGrid.PivotGridModule();
+            this.pivotGridAspNetModule = new DevExpress.ExpressApp.PivotGrid.Web.PivotGridAspNetModule();
+            this.reportsModuleV2 = new DevExpress.ExpressApp.ReportsV2.ReportsModuleV2();
+            this.reportsAspNetModuleV2 = new DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2();
+            this.treeListEditorsModuleBase = new DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase();
+            this.treeListEditorsAspNetModule = new DevExpress.ExpressApp.TreeListEditors.Web.TreeListEditorsAspNetModule();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.validationAspNetModule = new DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule();
+            this.viewVariantsModule = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            //
+            // reportsModuleV2
+            //
+            this.reportsModuleV2.EnableInplaceReports = true;
+            this.reportsModuleV2.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+            this.reportsModuleV2.ShowAdditionalNavigation = false;
+            this.reportsAspNetModuleV2.ReportViewerType = DevExpress.ExpressApp.ReportsV2.Web.ReportViewerTypes.HTML5;
+            this.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+            // 
+            // TestsAspNetApplication
+            // 
+            this.ApplicationName = "IntecoAG.XafExt.Tests";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.objectsModule);
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.pivotChartModuleBase);
+            this.Modules.Add(this.pivotChartAspNetModule);
+            this.Modules.Add(this.pivotGridModule);
+            this.Modules.Add(this.pivotGridAspNetModule);
+            this.Modules.Add(this.reportsModuleV2);
+            this.Modules.Add(this.reportsAspNetModuleV2);
+            this.Modules.Add(this.treeListEditorsModuleBase);
+            this.Modules.Add(this.treeListEditorsAspNetModule);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.validationAspNetModule);
+            this.Modules.Add(this.viewVariantsModule);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.TestsAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
